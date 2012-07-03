8238c2c03ceee26ec7dbc3236980810b102d505a
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2012-07-03 17:11:43 +0700

    Reinitialized app without test-unit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Milky::Application.config.secret_token = 'f1e330dc1db111579838d83f0805aac3dc079687bca55c9576708e79efbcc233752e94c2206d3540f38a26069b02cdefeff76bbeab8fcfa567eee7dbf26ea2e6'
+Milky::Application.config.secret_token = '1ec1611b9c1a0a3fcbff09cfe7bbde01f75599d3b70dd525042b80b00ceea16b6ff6b9645036adaaa7f5ba91bfe42ddb79046f209f1e4e56b93b6453e25dcad0'
