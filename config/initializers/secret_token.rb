e10da6caba3382a685b6ea480572e1ded2dcba9a
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2012-07-03 16:59:58 +0700

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milky::Application.config.secret_token = 'f1e330dc1db111579838d83f0805aac3dc079687bca55c9576708e79efbcc233752e94c2206d3540f38a26069b02cdefeff76bbeab8fcfa567eee7dbf26ea2e6'
